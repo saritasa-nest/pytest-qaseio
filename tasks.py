d164d1b92fdb789d03b29155fd0f6afcd478709c
Stanislav Khlud <stanislav.khlud@saritasa.com>
2026-05-06 12:29:49 +0700

    Sync with boilerplate

--- tasks.py
+++ tasks.py
@@ -26,6 +26,10 @@ ns.configure(
         },
         "saritasa_invocations": saritasa_invocations.Config(
             project_name="pytest-qaseio",
+            pre_commit=saritasa_invocations.PreCommitSettings(
+                entry="prek",
+                default_hook_stage="pre-push",
+            ),
         ),
     },
 )
